af861d7b81e46e2f09d2f4d8eca3ed9a70603d3c
yoonjoo.jang <yoonjoo.jang@blocko.io>
2018-09-18 12:34:36 +0900

    solidity update

--- contracts/Ethboard.sol
+++ contracts/Ethboard.sol
@@ -3,38 +3,71 @@ pragma solidity ^0.4.18;
 
 contract ethBoard {
     
-    event AddedPost(uint uid);
+    event AddedPost(uint postID, string title, string content, string ownerId, string firstName, string lastName, address ownerAccount);
 
     struct Post {
         uint uid;
         string title;
         string content;
-        string ownerName;
+        string ownerId;
+        string ownerFirstName;
+        string ownerLastName;
         address ownerAccount;
     }
 
-    uint numPosts; 
-   
-    mapping (uint => Post) posts;
+    uint lastPostId;
+    uint numPosts;
+    uint numOwnerAccount;
+
+    uint[] postIds;
+    address[] ownerAccounts;
+
+    mapping (uint => Post) posts;       
+    mapping (address => uint[]) userToPostMap;
+
     
     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
      *  These functions perform transactions, editing the mappings *
      * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-    function addPost(string title, string content, string ownerName) public {
+    function addPost(string title, string content, string ownerId, string ownerFirstName, string ownerLastName) public {
         // user is the return variable
-        uint postID = numPosts++;
+        uint postID = lastPostId++;
+        numPosts++;
         // Create new user Struct with name and saves it to storage.
-        posts[postID] = Post(postID, title, content, ownerName, msg.sender);
-        emit AddedPost(postID);
+        posts[postID] = Post(postID, title, content, ownerId, ownerFirstName, ownerLastName, msg.sender);
+        if(userToPostMap[msg.sender].length == 0) {
+            ownerAccounts.push(msg.sender);
+            numOwnerAccount++;
+        }
+        userToPostMap[msg.sender].push(postID);
+        postIds.push(postID);
+        emit AddedPost(postID, title, content, ownerId, ownerFirstName, ownerLastName, msg.sender);
+    }
+
+    function getAllPostIds() public view returns(uint[]) {
+        return postIds;
+    }
+
+    function getAllOwnerAccount() public view returns(address[]) {
+        return ownerAccounts;
     }
 
     function getNumOfPosts() public view returns(uint) {
         return numPosts;
     }
 
+    function getNumOfOwnerAccount() public view returns(uint) {
+        return numOwnerAccount;
+    }
+
     // returns user information, including its ID, name, and description
-    function getPost(uint uid) public view returns (uint, string, string, string, address) {
-        return (uid, posts[uid].title, posts[uid].content, posts[uid].ownerName, posts[uid].ownerAccount);
+    function getPost(uint uid) public view returns (uint, string, string, string, string, string, address) {
+        return (uid, posts[uid].title, posts[uid].content, posts[uid].ownerId, posts[uid].ownerFirstName ,posts[uid].ownerLastName, posts[uid].ownerAccount);
+    }
+
+    // returns post IDs by owner account
+    function getPostIdsByAccount(address ownerAccount) public view returns (uint[]) {
+        return userToPostMap[ownerAccount];
     }
 }
--- test/ethboard_test.js
+++ test/ethboard_test.js
@@ -19,14 +19,14 @@ contract('ethboardTest', function(accounts) {
 
   it('add post', function(done){
     ethBoard.deployed().then(async function(instance) {
-        const data = await instance.addPost('test1', 'test contest', 'testUser');
+        const data = await instance.addPost('post1', 'post1 contest', 'testID1', 'first1', 'second1');
         done();
    });
 });
 
 it('add post2', function(done){
   ethBoard.deployed().then(async function(instance) {
-      const data = await instance.addPost('test2', 'test contest2', 'testUser2');
+      const data = await instance.addPost('post2', 'post2 contest', 'testID2', 'first2', 'second2');
       done();
  });
 });
@@ -38,14 +38,42 @@ it('get numbaer of posts', function(done){
       done();
  });
 });
+
+it('get numbaer of account', function(done){
+  ethBoard.deployed().then(async function(instance) {
+      const ethBoard = await instance.getNumOfOwnerAccount.call();
+      console.log(ethBoard);
+      done();
+ });
+});
+
+it('get All PostIds', function(done){
+  ethBoard.deployed().then(async function(instance) {
+      const ethBoard = await instance.getAllPostIds.call();
+      console.log(ethBoard);
+      done();
+ });
+});
+
+it('get All OwnerAccount', function(done){
+  ethBoard.deployed().then(async function(instance) {
+      const ethBoard = await instance.getAllOwnerAccount.call();
+      console.log(ethBoard);
+      done();
+ });
+});
+
+
 it('get post', function(done){
   ethBoard.deployed().then(async function(instance) {
       const post = await instance.getPost(0);
       console.log("ID: " ,post[0].toNumber());
       console.log("Title: " ,post[1]);
       console.log("Content: " ,post[2]);
-      console.log("Owner name " ,post[3]);
-      console.log("Owner account " ,post[4]);
+      console.log("Owner Id " ,post[3]);
+      console.log("Owner first name " ,post[4]);
+      console.log("Owner last name " ,post[5]);
+      console.log("Owner account " ,post[6]);
       done();
  });
 });
@@ -55,9 +83,13 @@ it('get post', function(done){
       console.log("ID: " ,post[0].toNumber());
       console.log("Title: " ,post[1]);
       console.log("Content: " ,post[2]);
-      console.log("Owner name " ,post[3]);
-      console.log("Owner account " ,post[4]);
+      console.log("Owner Id " ,post[3]);
+      console.log("Owner first name " ,post[4]);
+      console.log("Owner last name " ,post[5]);
+      console.log("Owner account " ,post[6]);
       done();
  });
 });
+
+
 });
